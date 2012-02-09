53be1aee232cbb35423ec1d1184a9a265372af8d
nickklosterman <nick_klosterman@yahoo.com>
2012-02-09 17:04:33 -0500

    corrected CopyGimpItemsToGimpDirectory.sh

--- CopyGimpItemsToGimpDirectory.sh
+++ CopyGimpItemsToGimpDirectory.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cp -r scripts/ $HOME/.gimp-2.6/scripts/
-cp -r palettes/ $HOME/.gimp-2.6/palettes/
+cp -r scripts/ $HOME/.gimp-2.6/
+cp -r palettes/ $HOME/.gimp-2.6/
+#if copy to .gimp-2.6/palettes/ then since the palettes are in a palette dir we have two nested palette dirs
