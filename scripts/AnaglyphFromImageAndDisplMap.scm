16c3457185bb124b7967e0bc55b9bc0159536845
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-03-15 22:59:16 -0400

    updated AnaglyphFromImageAndDisplMap.scm, now teh rescale checkbox works, auto sets proper image type to rgb, makes layers full opacity incase had some not 100pct, turns displ map layer to not visible so after scrpit is done the anaglyph is showing.

--- scripts/AnaglyphFromImageAndDisplMap.scm
+++ scripts/AnaglyphFromImageAndDisplMap.scm
@@ -1,11 +1,12 @@
 (define (script-fu-anaglyph_displ_map theImage drawable displacement_offset_x displacement_offset_y  displacement_type edge_behavior rescale_levels levelsmode)
 
   (let* (
- (theLayersList (cadr (gimp-image-get-layers theImage)))
-        (displmapLyr (aref theLayersList 0)) ;if you have multiple layers the image map needs to be the topmost layer
-        (theBackgroundLayer (aref theLayersList 1))
+	 (theLayersList (cadr (gimp-image-get-layers theImage)))
+	 (displmapLyr (aref theLayersList 0)) ;if you have multiple layers the image map needs to be the topmost layer
+	 (theBackgroundLayer (aref theLayersList 1))
 	 (redLyr (car (gimp-layer-copy drawable TRUE)))
 	 (cyanLyr (car (gimp-layer-copy drawable TRUE)))
+	 (isRGBmode (car (gimp-drawable-is-rgb drawable)))
 	 (points (make-vector 8 'byte))
          )
 
@@ -19,22 +20,41 @@
     (aset points 7 0)
 
 
+
+					;TODO:auto turn the displ map layer visibility off
+;DONE: 2012-03-15 TODO:make sure opacity 100%
+
 					;create undo group 
     (gimp-image-undo-group-start theImage)
 
-;(if (rescale_levels)
-    (gimp-levels displmapLyr levelsmode 0 255 1.0 0 128)
+    (if ( = isRGBmode FALSE)
+	(gimp-image-convert-rgb theImage) ;the parens are crucial or it will fail
+	)
+;make sure opacity at 100%
+(gimp-layer-set-opacity drawable 100 )
+(gimp-layer-set-opacity displmapLyr 100 )
+(gimp-layer-set-opacity theBackgroundLayer 100 )
 
-                                      ;create Cyan layer                                         
+
+;creating copies here bc if created when program called they will cause an error if we need to convert to RGB. image will be RGB but layers will be of diff type.
+	(set! redLyr (car (gimp-layer-copy drawable TRUE ))) 
+	(set! cyanLyr (car (gimp-layer-copy drawable TRUE ))) 
+
+
+					;I had this commented out even though we want to switch based on its input 2012-03-14 , for some reason I can't get this if statment to work. I had the data type wrong. Works now.
+    (if ( = rescale_levels TRUE)
+	(gimp-levels displmapLyr levelsmode 0 255 1.0 0 128)
+	) ;close the if rescale levels
+					;create Cyan layer                                         
     (gimp-image-add-layer theImage cyanLyr -1)
-    (gimp-drawable-set-name cyanLyr "Wiggle1 (100 ms)")
+    (gimp-drawable-set-name cyanLyr "Cyan Layer")
     
-                                    ;create Red layer                                          
+					;create Red layer                                          
     (gimp-image-add-layer theImage redLyr -1)
-    (gimp-drawable-set-name redLyr "Wiggle2 (100 ms)")
+    (gimp-drawable-set-name redLyr "Red Layer")
     (gimp-layer-set-mode redLyr SCREEN-MODE)
 
-                                      ;mask channels                                                                                      
+					;mask channels                                                                                      
     (gimp-curves-spline cyanLyr 1 8 points)
     (gimp-curves-spline redLyr 2 8 points)
     (gimp-curves-spline redLyr 3 8 points)
@@ -46,14 +66,18 @@
 
 
 					;merge layers down to clean up and create the single anaglyph image
+    
     (gimp-image-merge-down theImage redLyr 2)
 
-		;create undo group 
+					;turn off visibility of 
+(gimp-layer-set-visible displmapLyr FALSE )
+					;end undo group 
     (gimp-image-undo-group-end theImage)
 
     (gimp-displays-flush)
     )
   )
+
 (script-fu-register "script-fu-anaglyph_displ_map"
                     _"<Image>/Script-Fu/AnaglyphTools/Anaglyph-from-Image-and-Displ-Map"
                     "Processes an image and its depth map creating an anaglyph. Topmost layer = depth map, background layer selected"
@@ -63,12 +87,12 @@
                     "*"
                     SF-IMAGE        "Image"     0
                     SF-DRAWABLE     "Drawable"  0
-		    SF-ADJUSTMENT "X/Radial Displacement Offset (pixels)" (list 20 0 60 1 10 0 SF-SLIDER )
+		    SF-ADJUSTMENT "X/Radial Displacement Offset (pixels)" (list 10 0 60 1 10 0 SF-SLIDER )
 		    SF-ADJUSTMENT "Y/Tangent Displacement Offset (pixels)" (list 0 0 60 1 10 0 SF-SLIDER )
 		    SF-OPTION "Displacement Mode"  '("Cartesian" "Polar");this doesn't do anything
 		    SF-OPTION "Edge Behavior"  '("Wrap" "Smear" "Black")
-  SF-TOGGLE     "Rescale Levels?"   FALSE
-SF-OPTION "Levels Mode"  '("Value" "Red" "Green" "Blue" "Al\
+		    SF-TOGGLE     "Rescale Levels?"   FALSE
+		    SF-OPTION "Levels Mode"  '("Value" "Red" "Green" "Blue" "Al\
 pha" "RGB")
 
                     )
